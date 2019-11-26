d5395520e8c9c58057321b7f96dfc4117c3d2dad
Neil Champakara <nchamp3@uic.edu>
2019-11-26 00:43:10 -0600

    Added weather info and images to cards

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+let card_pics = ['./pics/m1.jpg', './pics/m2.jpg', './pics/m3.jpg', './pics/m4.jpg', './pics/m5.jpg', './pics/m6.jpg']
+let index = 0;
+
 // instantiate all components
 window.mdc.autoInit();
 // create a reference to it
@@ -18,48 +21,46 @@ $(".mdc-tab").on("click", function() {
 
 var url = "https://api.nasa.gov/insight_weather/?feedtype=json&ver=1.0&api_key=1LuGzfFVR9LTX4tEIJTUf5flVkStXjBMA5pa5oIO";
 $.get(url,
-  function(data) {
-    $.each(data, function(i, v) {
-      console.log(v);
-
-    }); // end of the callback function and parameter list for $.each
+  data => {
+    data.sol_keys.forEach(sol => createCard(sol, data[sol]));
+    // $.each(data, function(i, v) {
+      // console.log(v);
+    //
+    // }); // end of the callback function and parameter list for $.each
   }); //  end of the callback function and parameter list for $.get
 
 
   // Create Cards
-  const createCard = () => {
+  const createCard = (sol, sol_obj) => {
     $('.mdc-layout-grid__inner').append("<div class=\"mdc-layout-grid__cell--span-3\">" +
       "<div class=\"mdc-card\">" +
         "<div class=\"mdc-card__primary-action\">" +
-          "<div class=\"mdc-card__media mdc-card__media--square\">" +
-            "<div class=\"mdc-card__media-content\">Title</div>" +
+          "<div class=\"mdc-card__media mdc-card__media--16-9\" style=\"background-image: url(&quot;" + card_pics[index] + "&quot;);\">" +
           "</div>" +
           // ... additional primary action content ...
-        "</div>" +
-        "<div class=\"mdc-card__actions\">" +
-          "<div class=\"mdc-card__action-buttons\">" +
-            "<button class=\"mdc-button mdc-card__action mdc-card__action--button\">" +
-              "<span class=\"mdc-button__label\">Action 1</span>" +
-            "</button>" +
-            "<button class=\"mdc-button mdc-card__action mdc-card__action--button\">" +
-              "<span class=\"mdc-button__label\">Action 2</span>" +
-            "</button>" +
-          "</div>" +
-          "<div class=\"mdc-card__action-icons\">" +
-            "<button class=\"material-icons mdc-icon-button mdc-card__action mdc-card__action--icon\" title=\"Share\">share</button>" +
-            "<button class=\"material-icons mdc-icon-button mdc-card__action mdc-card__action--icon\" title=\"More options\">more_vert</button>" +
+          "<div class=\"demo-card__primary\">" +
+            "<h1 class=\"demo-card__title mdc-typography mdc-typography--headline6\">Sol " + sol + "</h1>" +
+            "<h2 class=\"demo-card__subtitle mdc-typography mdc-typography--subtitle2\">Season: " + sol_obj.Season + "</h2>" +
+            "<h2 class=\"demo-card__subtitle mdc-typography mdc-typography--subtitle2\">High: " + Math.round(sol_obj.AT.mx) + "&deg;C | Low: " + Math.round(sol_obj.AT.mn) + "&deg;C</h2>" +
           "</div>" +
         "</div>" +
       "</div>" +
     "</div>");
 
+    if(index == 5){
+      index = 0;
+    }
+    else {
+      index++;
+    }
+
   }
 
-  createCard();
-  createCard();
-  createCard();
-  createCard();
-  createCard();
-  createCard();
-  createCard();
-  createCard();
+  // createCard();
+  // createCard();
+  // createCard();
+  // createCard();
+  // createCard();
+  // createCard();
+  // createCard();
+  // createCard();
