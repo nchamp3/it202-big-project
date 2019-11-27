96ea8c9db7f55fbafc3c50f34666725c26b1eb60
Neil Champakara <nchamp3@uic.edu>
2019-11-27 12:35:43 -0600

    Added scripts to get photos and create cards

--- scripts.js
+++ scripts.js
@@ -5,6 +5,8 @@ let index = 0;
 window.mdc.autoInit();
 // create a reference to it
 const tabbar = document.querySelector('.mdc-tab-bar').MDCTabBar;
+const sol_text_field = document.querySelector('.mdc-text-field').MDCTextField;
+const rover_select_field =  document.querySelector('.mdc-select').MDCSelect;
 
 const hideScreens = () => {
   $(".content").hide();
@@ -16,23 +18,26 @@ $(".mdc-tab").on("click", function() {
   $(target).show();
 });
 
-// call a method
-// snackbar.open();
 
-var url = "https://api.nasa.gov/insight_weather/?feedtype=json&ver=1.0&api_key=1LuGzfFVR9LTX4tEIJTUf5flVkStXjBMA5pa5oIO";
-$.get(url,
-  data => {
-    data.sol_keys.forEach(sol => createCard(sol, data[sol]));
-    // $.each(data, function(i, v) {
-      // console.log(v);
-    //
-    // }); // end of the callback function and parameter list for $.each
-  }); //  end of the callback function and parameter list for $.get
+$("#img_search_button").on("click", function(){
 
 
-  // Create Cards
-  const createCard = (sol, sol_obj) => {
-    $('.mdc-layout-grid__inner').append("<div class=\"mdc-layout-grid__cell--span-3\">" +
+  console.log(rover_select_field.value);
+
+
+});
+
+const getWeather = () => {
+  var url = "https://api.nasa.gov/insight_weather/?feedtype=json&ver=1.0&api_key=1LuGzfFVR9LTX4tEIJTUf5flVkStXjBMA5pa5oIO";
+  $.get(url,
+    data => {
+      data.sol_keys.forEach(sol => createWeatherCard(sol, data[sol]));
+    });
+}
+
+  // Create Weather Cards
+  const createWeatherCard = (sol, sol_obj) => {
+    $('#weather_cards').append("<div class=\"mdc-layout-grid__cell--span-3\">" +
       "<div class=\"mdc-card\">" +
         "<div class=\"mdc-card__primary-action\">" +
           "<div class=\"mdc-card__media mdc-card__media--16-9\" style=\"background-image: url(&quot;" + card_pics[index] + "&quot;);\">" +
@@ -53,14 +58,32 @@ $.get(url,
     else {
       index++;
     }
-
   }
 
-  // createCard();
-  // createCard();
-  // createCard();
-  // createCard();
-  // createCard();
-  // createCard();
-  // createCard();
-  // createCard();
+  const getMRP = () => {
+    let url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=430&page=1&api_key=1LuGzfFVR9LTX4tEIJTUf5flVkStXjBMA5pa5oIO";
+    $.get(url,
+      function(data) {
+        $.each(data, function(i, v) {
+          $.each(v, function(j, k) {
+            createImageCard(k);
+          });
+        });
+      });
+  }
+
+  const createImageCard = (img) => {
+    $('#rover_img_cards').append("<div class=\"mdc-layout-grid__cell--span-3\">" +
+      "<div class=\"mdc-card\">" +
+        "<div class=\"mdc-card__primary-action\">" +
+          "<div class=\"mdc-card__media mdc-card__media--square\" style=\"background-image: url(&quot;" + img.img_src + "&quot;);\">" +
+          "</div>" +
+          // ... additional primary action content ...
+        "</div>" +
+      "</div>" +
+    "</div>");
+
+
+  }
+  getMRP();
+  // getWeather();
