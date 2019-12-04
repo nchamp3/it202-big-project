0494e19e82df45ecf707ebefbdff6fa2678bd0ad
Neil Champakara <nchamp3@uic.edu>
2019-12-03 23:56:21 -0600

    Service worker

---
+++ sw.js
@@ -0,0 +1,69 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+
+var CACHE_NAME = 'my-site-cache';
+var urlsToCache = [
+  './index.html',
+  './style.css',
+  './profileScript.js',
+  './scripts.js',
+  './sw.js',
+  './pics/m1.jpg',
+  './pics/m2.jpg',
+  './pics/m3.jpg',
+  './pics/m4.jpg',
+  './pics/m5.jpg',
+  './pics/m6.jpg',
+  './pics/mars_home.png'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      console.log('[Service Worker] Fetching resource: '+ e.request.url);
+
+      return r || fetch(e.request).then(function(response) {
+          return caches.open(cacheName).then(function(cache) {
+            console.log('[Service Worker] Caching new resource: '+e.request.url);
+            cache.put(e.request, response.clone());
+            return response;
+        });
+      });
+    })
+  );
+});
+
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request);
+//       }
+//     )
+//   );
+// });
